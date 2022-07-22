ac936a52e764a8bf122276dec9d887c3df3dfa83
Ammar <4opt@web.de>
2022-07-22 19:51:20 +0200

    this is my first commit

--- My_App/flutter_app1/lib/main.dart
+++ My_App/flutter_app1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// this is my first comment
 
 void main() {
   runApp(const MyApp());
@@ -22,6 +23,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
+
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
