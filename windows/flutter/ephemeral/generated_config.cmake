b290895ba25c90a9e2eb2146bcf5e8f95c7e7f38
mohamad <4opt@web.de>
2022-07-22 20:56:19 +0200

    ana hauit

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\mdugh\\Documents\\WSD\\dev\\firstapp" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter\\flutter"
+  "PROJECT_DIR=C:\\Users\\mdugh\\Documents\\WSD\\dev\\firstapp"
+  "FLUTTER_ROOT=C:\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\mdugh\\Documents\\WSD\\dev\\firstapp\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\mdugh\\Documents\\WSD\\dev\\firstapp"
+  "FLUTTER_TARGET=C:\\Users\\mdugh\\Documents\\WSD\\dev\\firstapp\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\mdugh\\Documents\\WSD\\dev\\firstapp\\.dart_tool\\package_config.json"
+)
